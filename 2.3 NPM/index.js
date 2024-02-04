dfeb0d545da3fa229e6be8ae812f1d189fb1fc95
KishoreXA-12 <dragonkrishoredbzgt@gmail.com>
2024-02-04 19:44:17 +0530

    4-2-2024

---
+++ 2.3 NPM/index.js
@@ -0,0 +1,6 @@
+// var generateName=require("sillyname");
+import generateName from "sillyname";
+var sillyname=generateName();
+console.log(`My name is ${sillyname}.`);
+import superheroes from "superheroes";
+console.log(`I am a ${superheroes.random()}`);
